b31116190a7528d329983ed366d58b221c3a40ff
carlortiz <mr.carlortiz@gmail.com>
2021-05-07 16:34:57 -0700

    Changes: * New feature: match ends after 9 turns and there is no winner * New feature: Filled boxes can no longer be written over

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,3 +1,6 @@
+MAX_TURNS = 9
+
+
 class Board:
 
     boxes = [" ", " ", " ",
@@ -15,30 +18,30 @@ class Board:
 
     @classmethod
     def check_winner(cls):
-        if Board.match_boxes(cls.boxes[0], cls.boxes[3], cls.boxes[6]):
+        if cls.match_boxes(cls.boxes[0], cls.boxes[3], cls.boxes[6]):
             return True
-        elif Board.match_boxes(cls.boxes[0], cls.boxes[4], cls.boxes[8]):
+        elif cls.match_boxes(cls.boxes[0], cls.boxes[4], cls.boxes[8]):
             return True
-        elif Board.match_boxes(cls.boxes[0], cls.boxes[1], cls.boxes[2]):
+        elif cls.match_boxes(cls.boxes[0], cls.boxes[1], cls.boxes[2]):
             return True
-        elif Board.match_boxes(cls.boxes[4], cls.boxes[1], cls.boxes[7]):
+        elif cls.match_boxes(cls.boxes[4], cls.boxes[1], cls.boxes[7]):
             return True
-        elif Board.match_boxes(cls.boxes[2], cls.boxes[4], cls.boxes[6]):
+        elif cls.match_boxes(cls.boxes[2], cls.boxes[4], cls.boxes[6]):
             return True
-        elif Board.match_boxes(cls.boxes[2], cls.boxes[5], cls.boxes[8]):
+        elif cls.match_boxes(cls.boxes[2], cls.boxes[5], cls.boxes[8]):
             return True
-        elif Board.match_boxes(cls.boxes[3], cls.boxes[4], cls.boxes[5]):
+        elif cls.match_boxes(cls.boxes[3], cls.boxes[4], cls.boxes[5]):
             return True
-        elif Board.match_boxes(cls.boxes[6], cls.boxes[7], cls.boxes[8]):
+        elif cls.match_boxes(cls.boxes[6], cls.boxes[7], cls.boxes[8]):
             return True
-
         return False
 
     @staticmethod
-    def match_boxes(one_box, two_box, three_box):
-        if " " in {one_box, two_box, three_box}:
+    def match_boxes(box_one, box_two, box_three):
+        if " " in {box_one, box_two, box_three}:
             return False
-        if one_box == two_box == three_box:
+
+        if box_one == box_two == box_three:
             return True
         return False
 
@@ -47,27 +50,17 @@ class Player:
     players = []
 
     def __init__(self):
-        self.name = None
-        self.is_winner = False
-        self.symbol = None
-
-    @staticmethod
-    def new_player():
         print("\nOkay, a new player will be made!")
-        player = Player()
-        player.name = player.get_name()
+        self.name = self.get_name()
 
         if len(Player.players) == 0:
-            player.symbol = player.get_symbol()
+            self.symbol = self.get_symbol()
         else:
-            if Player.players[0].symbol == "X":
-                player.symbol = "O"
-            else:
-                player.symbol = "X"
+            self.symbol = self.set_symbol()
+        print("Okay, your symbol is ", self.symbol)
 
-        print("Okay, your symbol is ", player.symbol)
-        Player.players.append(player)
-        return player
+        self.is_winner = False
+        Player.players.append(self)
 
     @staticmethod
     def get_name():
@@ -82,11 +75,15 @@ class Player:
                 print("Only 'X' or 'O' allowed.")
             else:
                 break
-
         return chosen_symbol.upper()
 
-    def player_turn(self):
-        print("Okay ", self.name, ",it is now your turn.")
+    def set_symbol(self):
+        if Player.players[0].symbol == "X":
+            return "O"
+        return "X"
+
+    @staticmethod
+    def player_turn():
         chosen_box = int(input("Choose box (1-9): "))
         return chosen_box - 1
 
@@ -96,18 +93,17 @@ intro = input("Hello, welcome to tic tac toe!")
 game_over = False
 
 print("Would you like to play single player or two player mode?")
-
 while True:
     game_mode = input("Choose mode (single/double): ")
 
     if game_mode == "single":
         print("\nOkay, single player it is!")
-        player_one = Player.new_player()
+        player_one = Player()
         break
     elif game_mode == "double":
         print("\nOkay, double player it is!")
-        player_one = Player.new_player()
-        player_two = Player.new_player()
+        player_one = Player()
+        player_two = Player()
         break
     else:
         print("Only 'single' or double' allowed.")
@@ -116,18 +112,25 @@ current_player = Player.players[0]
 turns_count = 0
 
 while not game_over:
-    if turns_count == 9:
-        print("Draw, there is no winner!")
-        game_over = True
-        break
-
     board = Board()
     board.display()
 
-    box = current_player.player_turn()
-    board.boxes[box] = current_player.symbol
+    if turns_count == MAX_TURNS:
+        print("\nDraw, there is no winner!")
+        game_over = True
+        break
 
-    print("The player ", current_player.name, " marked box #", box + 1)
+    print("Okay ", current_player.name, ",it is now your turn.")
+    while True:
+        box_number = current_player.player_turn()
+        box = board.boxes[box_number]
+
+        if box != "X" and box != "O":
+            box = current_player.symbol
+            break
+        print("You must choose an empty box.")
+
+    print("The player ", current_player.name, " marked box #", box_number + 1)
 
     current_player.is_winner = Board.check_winner()
 
